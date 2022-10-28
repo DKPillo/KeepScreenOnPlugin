2f4600fdbe1575ff9bc5b1fcabb2b8f26c88cf78
Salim Hermidas <salim@hermidas.ch>
2022-10-28 13:09:07 +0200

    Update JS Object creation

--- README.md
+++ README.md
@@ -14,9 +14,9 @@ cordova plugin add https://github.com/DKPillo/KeepScreenOnPlugin.git
 
 ```javascript
 // Set flag on current window
-cordova.KeepScreenOn.setKeepScreenOn();
+cordova.plugins.KeepScreenOn.setKeepScreenOn();
 
 // Clear flag on current window
-cordova.KeepScreenOn.cancelKeepScreenOn();
+cordova.plugins.KeepScreenOn.cancelKeepScreenOn();
 ```
 
--- www/keepScreenOn.js
+++ www/keepScreenOn.js
@@ -1,16 +1,16 @@
 var exec = require('cordova/exec');
 
 var emptyFn = function(){};
-var KeepScreenOn = {
-    setKeepScreenOn: function () {
-        exec(emptyFn, emptyFn, "KeepScreenOn", "SetKeepScreenOn", [""]);
-    },
-    cancelKeepScreenOn: function () {
-        exec(emptyFn, emptyFn, "KeepScreenOn", "CancelKeepScreenOn", [""]);
-    },
-    test: function () {
-        return "KeepScreenOn is available!";
-    }
+function KeepScreenOn() {}
+
+KeepScreenOn.prototype.setKeepScreenOn = function() {
+    exec(emptyFn, emptyFn, "KeepScreenOn", "SetKeepScreenOn", [""]);
+};
+KeepScreenOn.prototype.cancelKeepScreenOn = function() {
+    exec(emptyFn, emptyFn, "KeepScreenOn", "CancelKeepScreenOn", [""]);
+};
+KeepScreenOn.prototype.test = function() {
+    return "KeepScreenOn is available!";
 };
 
-module.exports = KeepScreenOn;
+module.exports = new KeepScreenOn();
